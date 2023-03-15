39a7b7e539727dfde5239a8c4b0f2cbb4bbd345b
Aminat Animashaun <aminatanimashaun31@aminatanimashaun31@gmail.com>
2023-03-15 01:14:55 -0700

    javascript-object-scope

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,25 +1,8 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
 
-module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let col = 0; col < this.height; col += 1) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    [this.width, this.height] = [this.height, this.width];
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
   }
 };
